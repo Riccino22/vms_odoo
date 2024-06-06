bbec012060928dd1e2053823ae0901d8f8da666e
riccino <riccardorinojosam@gmail.com>
2024-06-06 00:00:05 -0300

    added de synonyms field

--- models/defs.py
+++ models/defs.py
@@ -1,34 +1,46 @@
 from langchain.prompts import PromptTemplate
 from langchain.chains.llm import LLMChain
 from langchain_groq import ChatGroq
-from langchain.prompts import PromptTemplate
-from langchain.chains.llm import LLMChain
-from langchain_groq import ChatGroq
-#from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import json
 
 load_dotenv()
 
 def translate_with_llm(word):
-    template = """Como se dice "{frase}" en ingles? Tu respuesta debe ser un objeto de python con la siguiente estructura:
-    (signo de llave de apertura)
-    "word" : "palabra a traducir",
-    "translation" : "palabra traducida al ingles",
-    "definition": "definicion de al menos 25 palabras de la palabra en cuestion, en ingles",
-    "examples" : ["Primer ejemplo de uso de la palabra en ingles","Segundo ejemplo de uso de la palabra en ingles", "Tercer ejemplo de uso de la palabra en ingles"] 
-    (signo de llave de cierre)
-    Solo debes responder con la estructura antes descrita, nada mas.
+    template = """I am going to give you this word or phrase: "{phrase}" and you will translate this word or phrase from its language to English. Your response should be a Python object with the following structure:
+    
+    (opening curly brace)
+    "word" : "word to translate",
+    "translation" : "word translated to English. If the word is already in English, then use the same word",
+    "category" : "the category of the word, that is, if it is a verb, a noun, etc. It should be one of the following options: 'Noun', 'Pronoun', 'Verb', 'Adjective', 'Adverb', 'Preposition', 'Conjunction', 'Interjection', 'Article', 'Determiner', 'Numeral', 'Phrase'",
+    "definition": "definition of at least 25 words of the word in question, in English",
+    "synonyms": "A list with at least one synonym of the word or phrase in English, separated by commas",
+    "examples" : ["First example of the word usage in English","Second example of the word usage in English", "Third example of the word usage in English"]
+    (closing curly brace)
+    
+    You should only respond with the structure described above, nothing more. If the word or phrase does not exist, then respond with a Python object with the following structure:
+    
+    (opening curly brace)
+    "word" : "False",
+    "translation" : "False",
+    "category" : "False",
+    "definition": "False",
+    "synonyms":"False",
+    "examples" : "False"
+    (closing curly brace)
+    
+    Remember, the word or phrase is: "{phrase}"
+
     """
     
     llm = ChatGroq(temperature=0, model_name="llama3-8b-8192")
-    prompt_template = PromptTemplate(input_variables=["frase"], template=template)
+    prompt_template = PromptTemplate(input_variables=["phrase"], template=template)
     chain = LLMChain(llm=llm, prompt=prompt_template)
-    respuesta = chain.invoke(input={
-        "frase": word,
+    response = chain.invoke(input={
+        "phrase": word,
     })
-    print(respuesta["text"])
-    return json.loads(respuesta["text"])
+    print(response["text"])
+    return json.loads(response["text"])
         
 """
 while True:
--- models/word.py
+++ models/word.py
@@ -1,4 +1,5 @@
 from odoo import fields, api, models
+from odoo.exceptions import ValidationError
 from . import defs
 
 class Word(models.Model):
@@ -7,11 +8,57 @@ class Word(models.Model):
     name = fields.Char("Word")
     translation = fields.Char("Translation", compute="_translate_word")
     definition = fields.Text("Definition", compute="_translate_word")
+    examples = fields.Text("Examples", compute="_translate_word")
+    category = fields.Char("Category", compute="_translate_word")
+    synonyms = fields.Char("Synonyms", compute="_translate_word")
+    language = fields.Selection([
+    ('mandarin', 'Mandarin'),
+    ('spanish', 'Spanish'),
+    ('english', 'English'),
+    ('hindi', 'Hindi'),
+    ('arabic', 'Arabic'),
+    ('bengali', 'Bengali'),
+    ('portuguese', 'Portuguese'),
+    ('russian', 'Russian'),
+    ('japanese', 'Japanese'),
+    ('western_punjabi', 'Western Punjabi'),
+    ('marathi', 'Marathi'),
+    ('telugu', 'Telugu'),
+    ('wu_chinese', 'Wu Chinese'),
+    ('turkish', 'Turkish'),
+    ('korean', 'Korean'),
+    ('french', 'French'),
+    ('german', 'German'),
+    ('vietnamese', 'Vietnamese'),
+    ('tamil', 'Tamil'),
+    ('urdu', 'Urdu'),
+    ('javanese', 'Javanese'),
+    ('italian', 'Italian'),
+    ('egyptian_arabic', 'Egyptian Arabic'),
+    ('gujarati', 'Gujarati'),
+    ('iranian_persian', 'Iranian Persian'),
+    ('bhojpuri', 'Bhojpuri'),
+    ('southern_min', 'Southern Min'),
+    ('haitian_creole', 'Haitian Creole'),
+    ('hungarian', 'Hungarian'),
+    ('malay', 'Malay')
+], string='Language')
+
     print("-----------------------------------------------")
     @api.onchange("name")
     def _translate_word(self):
         for record in self:
-            word_data = defs.translate_with_llm(record.name)
-            record.definition = word_data["definition"]
-            record.translation = word_data["translation"]
-            print(word_data["examples"])
+            if record.name != False:
+                word_data = defs.translate_with_llm(record.name)
+                if word_data["translation"] and word_data["definition"] != "False":
+                    record.definition = word_data["definition"]
+                    record.translation = word_data["translation"]
+                    record.category = word_data["category"]
+                    record.synonyms = word_data["synonyms"]
+                                    
+                    examples = ""
+                    for example in word_data["examples"]:
+                        examples += f"- {example}\n"
+                    record.examples = examples
+                else:
+                    raise ValidationError("The word or phrase can't be proccessed")
